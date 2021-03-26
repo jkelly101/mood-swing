2d10d145b88b557f01669312398fcb44a62de471
Jacob Hohing <jakehohing@yahoo.com>
2021-03-25 20:06:25 -0400

    Setting env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
