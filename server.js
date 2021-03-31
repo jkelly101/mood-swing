be33bfa9245b7c0d16c9dbf93b654b8f36342bd0
jkelly101 <jenLkelly@gmail.com>
2021-03-30 20:20:15 -0400

    removed cors

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var express = require("express");
 var session = require("express-session");
 
 // Testing Out API Packages
-var cors = require('cors');
-var axios = require('axios');
+var axios = require("axios");
 
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
@@ -18,7 +17,6 @@ var db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 
-app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -28,7 +26,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -37,8 +37,12 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
