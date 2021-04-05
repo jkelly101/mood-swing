63048b9f5b363499d1abe9fcfaf086e5fe747d19
kierajcullen <kierajcullen@gmail.com>
2021-04-04 23:28:43 -0400

    readme, env api keys

--- public/js/mood2.js
+++ public/js/mood2.js
@@ -1,14 +1,11 @@
-// on click function (may require ajax call)
-$(".mood-click").on("click", (event) => {
-  //
-  event.preventDefault();
-  let mood = $(event.target).attr("id");
-  window.location.replace(`/${mood}`);
+// // on click function (may require ajax call)
+// $(".mood-click").on("click", (event) => {
+//   //
+//   event.preventDefault();
+//   let mood = $(event.target).attr("id");
+//   window.location.replace(`/${mood}`);
+// });
 
-
-
-});
-
-// send something to the route
-// $("#sad");
-// theoretically we can set a mood in local storage... can we grab from the background, big mystery
+// // send something to the route
+// // $("#sad");
+// // theoretically we can set a mood in local storage... can we grab from the background, big mystery
--- public/js/sad.js
+++ public/js/sad.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  var giphyURL =
-    "https://api.giphy.com/v1/gifs/search?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy&limit=12&q=sad";
-
-  $.get(giphyURL).then(function (data) {
+  // var giphyURL =
+  // "https://api.giphy.com/v1/gifs/search?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy&limit=12&q=unhappy";
+  $.get(`/api/giphy/unhappy`).then(function (data) {
     let counter = 0;
+    console.log(data);
     let containerWidth = $(".gif-row").width();
     let containerFill = 0;
 
@@ -60,15 +60,15 @@ $(document).ready(function () {
     }
 
     theMood = $(this).attr("id");
-    theNewUrl =
-      "https://api.giphy.com/v1/gifs/search?api_key=RANHjz2L1Drs7AUTUKTyWRWnP2iuiQpy&limit=12&q=" +
-      theMood;
+    // theNewUrl =
+    //   `https://api.giphy.com/v1/gifs/search?api_key=${giphy_APIKEY}&limit=12&q=` +
+    //   theMood;
 
     $(".gif-row img").attr("src", "");
 
     $(".spinner-border").show();
 
-    $.get(theNewUrl).then(function (data) {
+    $.get(`/api/giphy/${theMood}`).then(function (data) {
       let counter = 0;
       let containerWidth = $(".gif-row").width();
       let containerFill = 0;
@@ -112,6 +112,26 @@ $(document).ready(function () {
       $(".books" + i).attr("src", imageURL);
       $(".book-title-" + i).text(title);
       $(".book-desc-" + i).text(desc);
+    }
+  });
+  $.get("/api/movies/sad").then(function (data) {
+    console.log(data.movies.results);
+    let movieArray = data.movies.results;
+    let movieDiv = $("#movie-div");
+
+    for (let i = 0; i < 2; i++) {
+      console.log(movieArray[i]);
+      let title = movieArray[i].title;
+      let poster = `http://image.tmdb.org/t/p/w185/${movieArray[i].poster_path}`;
+      let overview = movieArray[i].overview;
+      let currentMovie = `<div class="card"style="width: 40%"> 
+        <img src=${poster} class="card-img-top" alt="movie poster"/>
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${overview}</p>
+        </div>
+      </div>`;
+      movieDiv.append(currentMovie);
     }
   });
 });
